152372163435dd04f5e48de33f59ddf177282efc
Aaron Wan <aaronwan@stanford.edu>
2023-11-12 16:18:03 -0800

    run experiments instructions

---
+++ run_experiments.py
@@ -0,0 +1,11 @@
+"""
+The run_experiments.py file must run the training and inference steps listed above and write out results.json, where results.json exactly matches this format:
+
+{
+    "loss": 100.0,
+    "inference_latency_1": 100.0,
+    "inference_latency_12": 100.0, 
+    "training_throughput_4": 0.0,
+    "training_throughput_12": 0.0
+}
+"""
