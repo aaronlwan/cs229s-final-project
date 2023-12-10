337fc4a1c2d403d053386f93c7879d156b393447
Aaron Wan <aaronwan@stanford.edu>
2023-12-10 13:22:43 -0800

    experiments

--- run_experiments.py
+++ run_experiments.py
@@ -345,6 +345,8 @@ def load_model(model_path, device='cuda'):
             if k.startswith(unwanted_prefix):
                 state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
         model.load_state_dict(state_dict)
+    else:
+        model = torch.load(model_path, map_location=device)
     model.to(device)
     return model
 
